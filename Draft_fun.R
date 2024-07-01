af53ce774bfdc06db90f4aaec5eb3146bc6a72e9
mnoslier <matthieu.noslier@ensae.fr>
2024-07-01 19:52:29 +0000

    Lundi soir

--- Draft_fun.R
+++ Draft_fun.R
@@ -11,5 +11,16 @@ fun_ind <- function(x,y){
   as.numeric(x > y)
 }
 
+install.packages("pracma") # double integral
+library(pracma)
+library(data.table) # for shift
+
 test <- integral2(fun_ind, 0, 1000, 0, 1000)$Q 
 # 509148 if x >= y; 487316.7 if x > y
+
+
+# f_ind(x,y)^2 = f_ind(x,y)
+# varTHEO_x <- integral2(f_x, 0, 1000, 0, 1000)$Q/p - (1/n)*(integral2(f_x, 0, 1000, 0, 1000)$Q^2)
+# varTHEO_x
+# Theoretical variance: 2498391345, i.e. around 2.498e+09
+# Close to the empirical one: great
